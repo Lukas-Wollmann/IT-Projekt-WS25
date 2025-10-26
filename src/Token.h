ac3845a125d5e1169edf190caca2d5a3d77cde72
Lukas Wollmann <lukaswollmann03@gmx.de>
2025-10-26 16:21:11 +0100

    fixed Token

--- src/Token.h
+++ src/Token.h
@@ -1,20 +1,29 @@
 #pragma once
-#include <string>
-#include <iostream>
 
-enum class TokenType 
+#include <iostream>
+#include <string>
+
+enum class TokenType
 {
-	IDENTIFIER, STRING_LITERAL, NUMERIC_LITERAL, CHAR_LITERAL, KEYWORD, OPERATOR, SEPERATOR
+	IDENTIFIER,
+	STRING_LITERAL,
+	NUMERIC_LITERAL,
+	CHAR_LITERAL,
+	KEYWORD,
+	OPERATOR,
+	SEPERATOR
 };
 
 std::ostream &operator<<(std::ostream &os, TokenType type);
 
-struct Token 
-{
-public: 
+struct Token {
+  public:
 	const TokenType type;
 	const std::string lexeme;
 	const size_t line, column, index;
+
+	Token(TokenType type, std::string lexeme, size_t line, size_t column, size_t index)
+		: type(type), lexeme(std::move(lexeme)), line(line), column(column), index(index) {};
 };
 
 std::ostream &operator<<(std::ostream &os, const Token &t);
