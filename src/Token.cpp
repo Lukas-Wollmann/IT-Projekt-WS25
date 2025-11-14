ba3c0cace8fe303e729d24ba5796e300fcc42a4c
Mcpaslp <mcpaslp@gmail.com>
2025-11-14 19:37:39 +0100

    added os stream for ErrorTypeToken

--- src/Token.cpp
+++ src/Token.cpp
@@ -28,6 +28,23 @@ std::ostream &operator<<(std::ostream &os, TokenType type)
     return os << "<UnknownTokenType>";
 }
 
+std::ostream &operator<<(std::ostream &os, ErrorTypeToken type)
+{
+    switch (type)
+    {
+        case ErrorTypeToken::NOT_ILLEGAL: return os << "NOT_ILLEGAL";
+        case ErrorTypeToken::UNTERMINATED_STRING: return os << "UNTERMINATED_STRING";
+		case ErrorTypeToken::SOLO_BACKSLASH_IN_CHAR_LITERAL: return os << "SOLO_BACKSLASH_IN_CHAR_LITERAL";
+		case ErrorTypeToken::INVALID_ESCAPE_SEQUENCE: return os << "INVALID_ESCAPE_SEQUENCE";
+		case ErrorTypeToken::MULTIPLE_CHAR_IN_CHAR_LITERAL: return os << "MULTIPLE_CHAR_IN_CHAR_LITERAL";
+		case ErrorTypeToken::UNTERMINATED_CHAR_LITERAL: return os << "UNTERMINATED_CHAR_LITERAL";
+		case ErrorTypeToken::EMPTY_CHAR_LITERAL: return os << "EMPTY_CHAR_LITERAL";
+		case ErrorTypeToken::UNTERMINATED_BLOCK_COMMENT: return os << "UNTERMINATED_BLOCK_COMMENT";
+		case ErrorTypeToken::ILLEGAL_IDENTIFIER: return os << "ILLEGAL_IDENTIFIER";
+    }
+    return os << "<UnknownIllegalType>";
+}
+
 std::ostream &operator<<(std::ostream &os, const Token &t) 
 {
     return os << "Token(" << t.type << ", " << t.lexeme << ", " << t.loc.line << ", " << t.loc.column << ", " << t.loc.index << ")";
