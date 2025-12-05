45770564aca77c59f92468082205be25e9e0d14f
Phillip Reiß <endrix1223@gmail.com>
2025-12-05 17:27:53 +0100

    fixed String bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,28 @@
 #include <fstream>
 
-#include "Lexer.h"
+#include "lexer/Lexer.h"
 #include "core/U8String.h"
 #include "utf8cpp/utf8.h"
 
 using it = utf8::iterator<std::u8string::const_iterator>;
 
 int main() {
-    std::ifstream file("test/TestCode/0_prototype_and_comments/Test_0_1.txt");
+    std::ifstream file("res/test.bla");
     std::string code;
     std::string line;
+    
     while (getline(file, line)) {
         code.append(line);
         code.append("\n");
     }
-    code.append("\n0");
+
+    
+    //code.append("\0");
+    
     for (auto t : code) {
         std::cout << std::hex << (int) (t) << ", ";
     }
+
     std::cout << code << "\n";
     std::cout << U8String(code) << "\n Test3";
 
