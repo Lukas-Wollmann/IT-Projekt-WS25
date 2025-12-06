20ac00d127256cac6b9a8b1ac00e0908dd7bb99b
Phillip Reiß <endrix1223@gmail.com>
2025-12-06 14:19:53 +0100

    added tests for compre visitor

---
+++ test/type/TestCompareVisitor.cpp
@@ -0,0 +1,250 @@
+#include "Doctest.h"
+#include "type/CompareVisitor.h"
+
+using namespace type;
+
+TEST_CASE("PrimitiveType: equal typenames means equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+
+	// Act
+	bool equal = *primitiveType1 == *primitiveType2;
+
+	// Assert
+	CHECK(equal);
+}
+
+TEST_CASE("PrimitiveType: different typenames means not equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32);
+
+	// Act
+	bool equal = *primitiveType1 == *primitiveType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("PointerType: equal underlying types means equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto ptrType1 = std::make_unique<PointerType>(std::move(primitiveType1));
+	auto ptrType2 = std::make_unique<PointerType>(std::move(primitiveType2));
+
+	// Act
+	bool equal = *ptrType1 == *ptrType2;
+
+	// Assert
+	CHECK(equal);
+}
+
+TEST_CASE("PointerType: different underlying types means not equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32);
+	auto ptrType1 = std::make_unique<PointerType>(std::move(primitiveType1));
+	auto ptrType2 = std::make_unique<PointerType>(std::move(primitiveType2));
+
+	// Act
+	bool equal = *ptrType1 == *ptrType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("ArrayType: equal size and equal type means equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto arrType1 = std::make_unique<ArrayType>(std::move(primitiveType1), 42);
+	auto arrType2 = std::make_unique<ArrayType>(std::move(primitiveType2), 42);
+
+	// Act
+	bool equal = *arrType1 == *arrType2;
+
+	// Assert
+	CHECK(equal);
+}
+
+TEST_CASE("ArrayType: different size and equal type means not equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto arrType1 = std::make_unique<ArrayType>(std::move(primitiveType1), 42);
+	auto arrType2 = std::make_unique<ArrayType>(std::move(primitiveType2), 67);
+
+	// Act
+	bool equal = *arrType1 == *arrType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("ArrayType: equal size and different type means not equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32);
+	auto arrType1 = std::make_unique<ArrayType>(std::move(primitiveType1), 42);
+	auto arrType2 = std::make_unique<ArrayType>(std::move(primitiveType2), 42);
+
+	// Act
+	bool equal = *arrType1 == *arrType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("ArrayType: different size and different type means not equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32);
+	auto arrType1 = std::make_unique<ArrayType>(std::move(primitiveType1), 42);
+	auto arrType2 = std::make_unique<ArrayType>(std::move(primitiveType2), 67);
+
+	// Act
+	bool equal = *arrType1 == *arrType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("ArrayType: unsized and sized array means not equal") {
+	// Arrange
+	auto primitiveType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto primitiveType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto arrType1 = std::make_unique<ArrayType>(std::move(primitiveType1), 42);
+	auto arrType2 = std::make_unique<ArrayType>(std::move(primitiveType2));
+
+	// Act
+	bool equal = *arrType1 == *arrType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("FunctionType: equal parameters and equal return types means equal") {
+	// Arrange
+	Vec<Box<const Type>> params1;
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+
+	auto retType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::Bool);
+	auto funcType1 = std::make_unique<FunctionType>(std::move(params1), std::move(retType1));
+
+	Vec<Box<const Type>> params2;
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+
+	auto retType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::Bool);
+	auto funcType2 = std::make_unique<FunctionType>(std::move(params2), std::move(retType2));
+
+	// Act
+	bool equal = *funcType1 == *funcType2;
+
+	// Assert
+	CHECK(equal);
+}
+
+TEST_CASE("FunctionType: different parameters and equal return types means not equal") {
+	// Arrange
+	Vec<Box<const Type>> params1;
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+
+	auto retType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::Bool);
+	auto funcType1 = std::make_unique<FunctionType>(std::move(params1), std::move(retType1));
+
+	Vec<Box<const Type>> params2;
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32));
+
+	auto retType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::Bool);
+	auto funcType2 = std::make_unique<FunctionType>(std::move(params2), std::move(retType2));
+
+	// Act
+	bool equal = *funcType1 == *funcType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("FunctionType: equal parameters and different return types means not equal") {
+	// Arrange
+	Vec<Box<const Type>> params1;
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32));
+
+	auto retType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::Bool);
+	auto funcType1 = std::make_unique<FunctionType>(std::move(params1), std::move(retType1));
+
+	Vec<Box<const Type>> params2;
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+
+	auto retType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto funcType2 = std::make_unique<FunctionType>(std::move(params2), std::move(retType2));
+
+	// Act
+	bool equal = *funcType1 == *funcType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("FunctionType: different parameters and different return types means not equal") {
+	// Arrange
+	Vec<Box<const Type>> params1;
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+	params1.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32));
+
+	auto retType1 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::Bool);
+	auto funcType1 = std::make_unique<FunctionType>(std::move(params1), std::move(retType1));
+
+	Vec<Box<const Type>> params2;
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::F32));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32));
+	params2.push_back(std::make_unique<PrimitiveType>(PrimitiveTypeKind::Char));
+
+	auto retType2 = std::make_unique<PrimitiveType>(PrimitiveTypeKind::I32);
+	auto funcType2 = std::make_unique<FunctionType>(std::move(params2), std::move(retType2));
+
+	// Act
+	bool equal = *funcType1 == *funcType2;
+
+	// Assert
+	CHECK(!equal);
+}
+
+TEST_CASE("ErrorType: two different error types are equal") {
+	// Arrange
+	auto errorType1 = std::make_unique<ErrorType>();
+	auto errorType2 = std::make_unique<ErrorType>();
+
+	// Act
+	bool equal = *errorType1 == *errorType2;
+
+	// Assert
+	CHECK(equal);
+}
+
+TEST_CASE("UnitType: two different error types are equal") {
+	// Arrange
+	auto unitType1 = std::make_unique<ErrorType>();
+	auto unitType2 = std::make_unique<ErrorType>();
+
+	// Act
+	bool equal = *unitType1 == *unitType2;
+
+	// Assert
+	CHECK(equal);
+}
