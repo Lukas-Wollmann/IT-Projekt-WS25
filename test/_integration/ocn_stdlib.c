726583a08939f1b3414d58e0755d751b44929a34
Phillip <endrix1223@gmail.com>
2026-05-12 16:18:38 +0000

    updated test lib

--- test/_integration/ocn_stdlib.c
+++ test/_integration/ocn_stdlib.c
@@ -47,6 +47,11 @@ void __panic_null_deref() {
 	abort();
 }
 
+void __panic_out_of_bounds() {
+	fputs("panic: array out of bounds\n", stderr);
+	abort();
+}
+
 OCN_I32 read_i32() {
 	OCN_I32 num;
 
@@ -149,6 +154,55 @@ void __sp_drop(void *ptr) {
 	if (cbptr->refCount == 0) {
 		if (cbptr->dtor)
 			cbptr->dtor(ptr);
+		free(cbptr);
+	}
+}
+
+typedef struct {
+	size_t refCount;
+	size_t arraySize;
+	size_t elementSize;
+	void (*dtor)(void *);
+} ArrayControlBlock;
+
+void *__arr_create(size_t elementSize, size_t arraySize, void (*dtor)(void *)) {
+	void *ptr = calloc(1, sizeof(ArrayControlBlock) + (elementSize * arraySize));
+	if (!ptr)
+		return NULL;
+
+	ArrayControlBlock *cbptr = (ArrayControlBlock *) ptr;
+	cbptr->refCount = 1;
+	cbptr->arraySize = arraySize;
+	cbptr->elementSize = elementSize;
+	cbptr->dtor = dtor;
+
+	return cbptr + 1;
+}
+
+void *__arr_copy(void *ptr) {
+	if (!ptr)
+		return NULL;
+
+	ArrayControlBlock *cbptr = (ArrayControlBlock *) ptr - 1;
+	cbptr->refCount++;
+	return ptr;
+}
+
+void __arr_drop(void *ptr) {
+	if (!ptr)
+		return;
+
+	ArrayControlBlock *cbptr = (ArrayControlBlock *) ptr - 1;
+	cbptr->refCount--;
+
+	if (cbptr->refCount == 0) {
+		if (cbptr->dtor) {
+			char *element_ptr = (char *) ptr;
+			for (size_t i = 0; i < cbptr->arraySize; i++) {
+				cbptr->dtor(element_ptr + (i * cbptr->elementSize));
+			}
+		}
+
 		free(cbptr);
 	}
 }
